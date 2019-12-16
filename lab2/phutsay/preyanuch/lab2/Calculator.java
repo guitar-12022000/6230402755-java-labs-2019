2625042f79e0300c32f4f5f655925accdd094c8f
guitar-12022000 <58907932+guitar-12022000@users.noreply.github.com>
2019-12-16 16:41:28 +0700

    Add lab2

--- lab2/phutsay/preyanuch/lab2/Calculator.java
+++ lab2/phutsay/preyanuch/lab2/Calculator.java
@@ -1,39 +1,46 @@
+/** 
+  *Calculator program
+*
+*Author: Preyanuch Phutsay
+* ID: 623040275-5
+* Sec: 2
+* Date: December 16, 2019
+**/
 package phutsay.preyanuch.lab2;
 import java.text.DecimalFormat;
 public class Calculator{
     private static DecimalFormat df2 = new DecimalFormat("#.##");
     public static void main(String[] args){
-        String operation = args[0]; 
+        String operator = args[0]; 
         double result = 0;
-        if (operation.equals("q")||operation.equals("Q")){
+        if (operator.equals("q")||operator.equals("Q")){
             System.out.println("Quitting the program");
         } else{
             double num1 = Double.parseDouble(args[1]);
             double num2 = Double.parseDouble(args[2]);
-            if (args.length == 3){
-                if (operation.equals("+")){
-                    result = num1 + num2;
-                    System.out.println(num1 + "+" + num2 + " = " + df2.format(result));
-                } else if (operation.equals("-")) {
-                    result = num1 - num2;
-                    System.out.println(num1 + "-" + num2 + " = " + df2.format(result));
-                } else if (operation.equals("*")){
-                    result = num1 * num2;
-                    System.out.println(num1 + "*" + num2 + " = " + df2.format(result));
-                } else {
-                    if (num2 == 0){
-                        System.out.println("The second operand can not be zero");
-                    } else {
-                        result = num1 / num2;
-                        System.out.println(num1 + "/" + num2 + " = " + df2.format(result));
-                    }
-                }
+            if(operator.equals("/") && args[2].equals("0")){
+                System.out.println("The second operand can not be zero");
+            }else if (operator.equals("+")){
+                result = num1 + num2;
+                System.out.println(num1 + "+" + num2 + " = " + df2.format(result));
+            } else if (operator.equals("-")) {
+                result = num1 - num2;
+                System.out.println(num1 + "-" + num2 + " = " + df2.format(result));
+            } else if (operator.equals("x")){
+                result = num1 * num2;
+                System.out.println(num1 + "x" + num2 + " = " + df2.format(result));
+            } else if  (operator.equals("/")){
+                result = num1/num2;
+                System.out.println(num1 + "/" + num2 + " = " + df2.format(result));
+            }
+                    
+        }
                 
 
-            }
-        }
+    }
+    
     
 
     }
-}
+
    
--- lab2/phutsay/preyanuch/lab2/VowelsCounter.java
+++ lab2/phutsay/preyanuch/lab2/VowelsCounter.java
@@ -1,16 +1,26 @@
+/** 
+  *This is VowelsCounter Program
+*
+*Author: Preyanuch Phutsay
+* ID: 623040275-5
+* Sec: 2
+* Date: December 16, 2019
+**/
 package phutsay.preyanuch.lab2;
 public class VowelsCounter{
     public static void main(String [] args){
-        String letter = args[0];
-        int count = 0;
-        for (int i=0 ; i<letter.length(); i++){
-            String vowel = String.valueOf(letter.charAt(i));
+        String letter = args[0]; //get letter argument
+        int count = 0; // set count at zero
+        for (int i=0 ; i<letter.length(); i++){//i is index of letter
+            String vowel = String.valueOf(letter.charAt(i));//convert letter.charAt(i) to String
         
-        if(vowel.equalsIgnoreCase("a") || vowel.equalsIgnoreCase("e") || vowel.equalsIgnoreCase("i") || vowel.equalsIgnoreCase("o") || vowel.equalsIgnoreCase("u")){
-            count += 1;
-        }
-        System.out.println("Total number of towels is " + count);
+            if(vowel.equalsIgnoreCase("a") || vowel.equalsIgnoreCase("e") || vowel.equalsIgnoreCase("i") ||
+                vowel.equalsIgnoreCase("o") || vowel.equalsIgnoreCase("u"))//accept LowerCase and UpperCase
+                {
+                count += 1;
+            }
+        
     
+        }System.out.println("Total number of towels is " + count);
     }
-}
 }
